2bee503f960b238f75c428f97008ae0e4d97a9dd
Blaine Bublitz <blaine@iceddev.com>
2014-02-18 23:55:40 -0700

    Allow options to be passed to the constructor

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict';
 
 var util = require('util');
+var extend = util._extend;
 var EventEmitter = require('eventemitter2').EventEmitter2;
 
-function Orchestrator () {
-	EventEmitter.call(this, {maxListeners:0});
-	this.tasks = {};
-	this.taskTimeout = 20*1000; // ms until the task fails
+function Orchestrator (opts) {
+	opts = extend({
+		wildcard: true,
+		maxListeners: 0,
+		tasks: {},
+		taskTimeout: 20*1000 // ms until the task fails
+	}, opts); // simple shallow clone
+
+	EventEmitter.call(this, opts);
+	this.tasks = opts.tasks;
+	this.taskTimeout = opts.taskTimeout;
 }
 util.inherits(Orchestrator, EventEmitter);
 
